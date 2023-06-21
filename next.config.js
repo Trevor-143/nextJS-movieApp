c801d5ee4833836692e4ba7ab898a51f35c6bd87
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-06-21 17:04:25 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["image.tmdb.org"],
+        disableStaticImages: true,
+    }
+}
+
+module.exports = nextConfig
